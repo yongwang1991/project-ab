75bdc6286f3dac685b2184ee6fc3ed1e250e9293
dchyii <hongyichong@gmail.com>
2024-10-13 01:15:47 +0800

    set up 2 col with chat window

--- main.py
+++ main.py
@@ -10,13 +10,45 @@ if not check_password():
 
 # st.write("hello world!")
 
-form = st.form(key="form")
-form.subheader("Prompt")
+## template prompt code ##
+# form = st.form(key="form")
+# form.subheader("Prompt")
 
-user_prompt = form.text_area("Enter your prompt here", height=200)
+# user_prompt = form.text_area("Enter your prompt here", height=200)
 
-if form.form_submit_button("Submit"):
-    st.toast(f"User Input Submitted - {user_prompt}")
-    response = llm.get_completion(user_prompt) 
-    st.write(response) 
-    print(f"User Input is {user_prompt}")
+# if form.form_submit_button("Submit"):
+#     st.toast(f"User Input Submitted - {user_prompt}")
+#     response = llm.get_completion(user_prompt) 
+#     st.write(response) 
+#     print(f"User Input is {user_prompt}")
+## template prompt code ##
+
+col1, col2 = st.columns(2)
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+with col1:
+    st.container(height=500)
+
+with col2:
+    chatcontainer = st.container(height=500)
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with chatcontainer.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        chatcontainer.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = f"Echo: {prompt}"
+        # Display assistant response in chat message container
+        with chatcontainer.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
