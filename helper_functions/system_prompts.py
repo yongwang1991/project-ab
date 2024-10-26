a3695d66873b7f76f9f9cb9188864e8c91f49502
dchyii <hongyichong@gmail.com>
2024-10-27 03:41:24 +0800

    completed the code for iterations through the sections

--- helper_functions/system_prompts.py
+++ helper_functions/system_prompts.py
@@ -11,7 +11,16 @@ You are a public sector procurement officer in the Singapore public service. You
 You are to generate an array of the key components of the user's requirement in the key "results" in the json. For example, for a dinner and dance event, the examples of the key components are ["venue", "food", "emcee", "photo booth"].  Based on the array, generate your response in proper sentence form and check if the user would like to amend or add on to your suggestions in the key "reply". The value to the key "next_step" shall be the word "setup_confirmation". 
 """,
 "setup_confirmation" : """
+Let's walk through this step by step. Firstly, check if the user is agreeable with the suggestions. 
+If the user confirms the list of key components without any further amendments, you are to return the original array in the key "results" in the json. Respond in a positive tone and inform the user that we shall move onto the first section of the requirements specifications. The value to the key "next_step" shall be "confirmed". 
 If the user suggests amendments to the list of key components, you are to amend the array of the key components of the user's requirement based on the latest user's prompt, and return the updated array in the key "results" in the json. Based on the original array and the updated array, inform the user in proper sentence form what you have amended and ask for confirmation in the key "reply". The value to the key "next_step" shall be the word "setup_confirmation". 
-If the user confirms the list of key components without any further amendments, you are to return the original array in the key "results" in the json. Response in a positive tone and inform the user that we shall move onto the first section of the draft. The value to the key "next_step" shall be "confirmed". 
+""",
+"initial_draft" : """
+Please draft the requirements specifications solely for this section as detailed as possible, in proper text paragraphs and not in json format. Any requirements that are specific to other sections of the requirements specifications shall not be included here. Any reference to yourself will be "Authority", while any reference to the supplier will be "Supplier". This draft shall be captured in the "results" key in the json. In the "reply" key, suggest to the user that you have provided a draft without including the results in your reply, and check if the user would like to amend your draft. The value to the key "next_step" shall be the word "confirmation". 
+""",
+"confirmation" : """
+Let's walk through this step by step. Firstly, check if the user is agreeable with the draft. If the sentiment is positive, meaning user is agreeable, return "confirmed" as the "next_step". If not, return "confirmation" as the "next_step". 
+If the sentiment is positive, you are to return the current draft in the proper text paragraphs form and not in json format in the "results" key. Respond in a positive tone and inform the user that we shall move onto the next section of the requirements specifications.
+If the user suggests amendments to the draft requirement specifications, you are to amend the draft based on the user's input, and return the updated draft in the "results" key. In the "reply" key, inform user that you have amended the draft and ask for confirmation. 
 """
 }
--- main.py
+++ main.py
@@ -25,13 +25,13 @@ if not check_password():
 #     print(f"User Input is {user_prompt}")
 ## template prompt code ##
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([2,1])
 
 # Initialize assistant
 if "background" not in st.session_state:
     st.session_state.background = [
         {"role": "system", "content": system_prompts.system_prompts["background"]},
-        {"role": "system", "content" : ""}
+        {"role": "system", "content" : "["",""]"}
     ]
 
 if "stage" not in st.session_state:
@@ -41,7 +41,7 @@ if "current_draft" not in st.session_state:
     st.session_state.current_draft = ""
 
 if "sow" not in st.session_state:
-    st.session_state.sow = {}
+    st.session_state.sow = []
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
@@ -109,11 +109,83 @@ with col2:
                 st.session_state.stage["status"] = "draft"
                 st.session_state.stage["current"] += 1
         
+        ##! first draft of the section
+        elif st.session_state.stage["current"] <= st.session_state.stage["max"] and st.session_state.stage["status"] == "draft":
+            # Display user message in chat message container
+            chatcontainer.chat_message("user").markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": f"{prompt}" })           
+
+            # Set up the prompt to llm
+            section_list = json.loads(st.session_state.background[1]["content"])
+            current_section = section_list[st.session_state.stage["current"] - 1]
+            system_instructions = [{"role": "system", "content": f"You are currently drafting the requirements specifications for the {current_section} section. " + system_prompts.system_prompts["initial_draft"]}]
+            built_prompt = st.session_state.background + st.session_state.messages + system_instructions
+            response_json = llm.get_completion_by_messages(built_prompt)
+
+            # Convert llm response in json back into python dictionary 
+            response = json.loads(response_json)
+
+            # Append llm reply to messages
+            with chatcontainer.chat_message("assistant"):
+                st.markdown(response["reply"])
+            st.session_state.messages.append({"role": "assistant", "content": response["reply"]})
+
+            # Update draft 
+            st.session_state.current_draft = response["results"]
+
+            # Change status to confirmation
+            st.session_state.stage['status'] = "confirmation"
+
+        ##! confirmation of draft
+        elif st.session_state.stage["current"] <= st.session_state.stage["max"] and st.session_state.stage["status"] == "confirmation":
+            # Display user message in chat message container
+            chatcontainer.chat_message("user").markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": f"{prompt}" })
+
+            # Set up the prompt to llm
+            section_list = json.loads(st.session_state.background[1]["content"])
+            current_section = section_list[st.session_state.stage["current"] - 1]
+            system_instructions = [{"role": "system", "content": f"You are currently drafting the requirements specifications for the {current_section} section. " + system_prompts.system_prompts["confirmation"]}]
+            built_prompt = st.session_state.background + st.session_state.messages + [{"role" : "assistant", "content" : f"current draft: {st.session_state.current_draft}"}] + system_instructions
+            print("built_prompt = ", built_prompt)
+            response_json = llm.get_completion_by_messages(built_prompt)
+
+            # Convert llm response in json back into python dictionary 
+            response = json.loads(response_json)
+            print("response = ", response)
+
+            # Append llm reply to messages
+            with chatcontainer.chat_message("assistant"):
+                st.markdown(response["reply"])
+            st.session_state.messages.append({"role": "assistant", "content": response["reply"]})
+
+            # Update draft  
+            st.session_state.current_draft = response["results"]
+
+            # If user is satisfied, move to the next section to start drafting
+            if response["next_step"] == "confirmed":
+                st.session_state.stage["status"] = "draft"
+                st.session_state.stage["current"] += 1
+                st.session_state.sow.append({current_section : st.session_state.current_draft})
+
         else:
             print("skipped till here")
 
-
 with col1:
     with st.container(height=500):
-        st.session_state
+        # section_list = json.loads(st.session_state.background[1]["content"])
+        # current_section = section_list[st.session_state.stage["current"] - 1]
+        st.markdown(st.session_state.current_draft)        
+
+with st.container(height=500):
+    for section in st.session_state.sow:
+        # st.markdown(section)
+        st.markdown(f"*{section.keys()}*")
+        st.markdown(section.values())
+
+
+with st.container(height=500):
+    st.session_state
 
