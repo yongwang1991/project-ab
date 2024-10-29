ebacf76a19c1388c3ca09e2abf49595063c93192
dchyii <hongyichong@gmail.com>
2024-10-29 23:56:29 +0800

    completed the entire sequence

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from helper_functions.utility import check_password
 from helper_functions import llm 
 from helper_functions import system_prompts
 
-st.title("Streamlit App")
+st.title("Specs Agent")
 
 # Check if the password is correct.  
 if not check_password():  
@@ -50,6 +50,7 @@ if "messages" not in st.session_state:
 
 
 with col2:
+    st.markdown("## Agent")
     chatcontainer = st.container(height=500)
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
@@ -66,7 +67,7 @@ with col2:
 
             ##! first engagement with llm to set up the main headers of the scope of work !##
             if st.session_state.stage["status"] == "setup":
-                print("setup")
+                ## print("setup")
                 # Set up the prompt to llm
                 system_instructions = [{"role": "system", "content": system_prompts.system_prompts["setup"]}]
                 built_prompt = [st.session_state.background[0]] + st.session_state.messages + system_instructions
@@ -85,18 +86,21 @@ with col2:
                 
             ##! check if user is agreeable with llm suggestions for the main headers !##
             elif st.session_state.stage["status"] == "setup_confirmation":
-                print("setup confirmation")
-                decision_json = llm.get_completion_by_messages(prompt)
+                ## print("setup confirmation")
+                user_prompt = [{"role" : "user", "content" : prompt}]
+                system_instructions = [{"role": "system", "content": system_prompts.system_prompts["confirmation"]}]
+                prompt_for_decision = st.session_state.messages + user_prompt + system_instructions
+                decision_json = llm.get_completion_by_messages(prompt_for_decision)
                 decision = json.loads(decision_json)
                 proceed_decision = decision["proceed"] #retrieve proceed decision
-                print("proceed_decision =", proceed_decision)
+                ## print("proceed_decision =", proceed_decision)
 
                 #* if user is agreeable with the main headers *#
-                if proceed_decision == True:
-                    print("setup confirmation true")
-                    response_json = llm.get_completion_by_messages(system_prompts.system_prompts["proceed"])
+                if proceed_decision == "True":
+                    ## print("setup confirmation true")
+                    response_json = llm.get_completion_by_messages([{"role" : "system", "content" : system_prompts.system_prompts["proceed"]}])
                     response = json.loads(response_json)
-                    print("setup confirmation reply: ", response)
+                    ## print("setup confirmation reply: ", response)
                     
                     # Append llm reply to the chat messages
                     with chatcontainer.chat_message("assistant"):
@@ -109,12 +113,12 @@ with col2:
                 
                 #* if user is NOT agreeable with the main headers *#
                 else:
-                    print("setup confirmation false")
+                    ## print("setup confirmation false")
                     system_instructions = [{"role": "system", "content": system_prompts.system_prompts["setup_amendment"]}]
                     built_prompt = st.session_state.background + st.session_state.messages + system_instructions
                     response_json = llm.get_completion_by_messages(built_prompt) 
                     response = json.loads(response_json)
-                    print("setup amendment reply: ", response)
+                    ## print("setup amendment reply: ", response)
 
                     # Update system info with user reqt
                     st.session_state.background[1]["content"] = json.dumps(response["results"])
@@ -146,14 +150,18 @@ with col2:
 
             ##! confirmation of drafting of each section !##
             elif st.session_state.stage["status"] == "confirmation":
-                decision_json = llm.get_completion_by_messages(prompt)
+                ## print("setup confirmation")
+                user_prompt = [{"role" : "user", "content" : prompt}]
+                system_instructions = [{"role": "system", "content": system_prompts.system_prompts["confirmation"]}]
+                prompt_for_decision = st.session_state.messages + user_prompt + system_instructions
+                decision_json = llm.get_completion_by_messages(prompt_for_decision)
                 decision = json.loads(decision_json)
                 proceed_decision = decision["proceed"] #retrieve proceed decision
-                print("proceed_decision =", proceed_decision)
+                ## print("proceed_decision =", proceed_decision)
 
                 #* if user is agreeable with the main headers *#
-                if proceed_decision == True:
-                    response_json = llm.get_completion_by_messages(system_prompts.system_prompts["proceed"])
+                if proceed_decision == "True":
+                    response_json = llm.get_completion_by_messages([{"role" : "system", "content" : system_prompts.system_prompts["proceed"]}])
                     response = json.loads(response_json)
                     
                     # Append llm reply to the chat messages
@@ -166,13 +174,13 @@ with col2:
                     st.session_state.stage["current"] += 1
 
                     # Update SOW with the confirmed draft
-                    st.session_state.sow.append([st.session_state.current_draft])
+                    st.session_state.sow.append(st.session_state.current_draft)
 
                 #* if user is NOT agreeable with the main headers *#
                 else:
                     section_list = json.loads(st.session_state.background[1]["content"])
                     current_section = section_list[st.session_state.stage["current"] - 1]
-                    system_instructions = [{"role": "system", "content": f"You are currently amending the requirements specifications for the {current_section} section. The current draft in demarcated by the tag <draft></draft> below. <draft>{st.session_state.current_draft}</draft> " + system_prompts.system_prompts["initial_draft"]}]
+                    system_instructions = [{"role": "system", "content": f"You are currently amending the requirements specifications for the {current_section} section. The current draft in demarcated by the tag <draft></draft> below. <draft>{st.session_state.current_draft}</draft> " + system_prompts.system_prompts["draft_amendment"]}]
                     built_prompt = st.session_state.background + st.session_state.messages + system_instructions
                     response_json = llm.get_completion_by_messages(built_prompt)
                     response = json.loads(response_json)
@@ -188,19 +196,36 @@ with col2:
                     # Change status to confirmation
                     st.session_state.stage['status'] = response["next_step"]
 
+            else: 
+                st.session_state.messages.append({"role": "assistant", "content": system_prompts.system_prompts["error"]})
+
         else:
-            print("end")
+            # print("end")
+            st.session_state.stage["status"] = "completed"
+            system_instructions = [{"role": "system", "content": system_prompts.system_prompts["completed"]}]
+            built_prompt = st.session_state.messages + system_instructions
+            response = llm.get_completion_by_messages(built_prompt)
+
+            with chatcontainer.chat_message("assistant"):
+                st.markdown(response["reply"])
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
 
 
 
 with col1:
+    st.markdown("## Current Draft")
     with st.container(height=500):
         st.markdown(st.session_state.current_draft)        
 
-with st.container(height=500):
-    st.session_state
+# with st.container(height=500):
+#     st.session_state
 
-with st.container(height=500):
+with st.container(height=700):
+    st.markdown("""
+                # Requirements Specification 
+                ---
+                 """)
     for section in st.session_state.sow:
         st.markdown(section)
         
