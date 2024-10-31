fff46f2c925434fcb4ccdfe57a9b48be764781ea
dchyii <hongyichong@gmail.com>
2024-10-31 11:37:55 +0800

    added disclaimers

--- main.py
+++ main.py
@@ -219,11 +219,28 @@ with tab1:
         with st.container(height=450):
             st.markdown(st.session_state.current_draft)        
 
+    with st.expander("Disclaimer"):
+        st.write("""
+            IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+            Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+            Always consult with qualified professionals for accurate and personalized advice.
+
+            """)
+
+
     # with st.container(height=500):
     #     st.session_state
 
 with tab2: 
     st.markdown("### Requirements Specification ")
+
+    with st.expander("Important Notice"):
+        st.write("""
+            The Requirements Specifications drafted by the agent is not based on training data of specifications drafted by any agencies within WOG. Use the Requirements Specifications with care. Officers are reminded to check for accuracy of the contents to ensure they meet the user's requirements, and are aligned with the principles of Government Procurement.
+            """)
+
     with st.container(height=500):
         for section in st.session_state.sow:
             st.markdown(section)
