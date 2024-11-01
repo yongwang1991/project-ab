fe118b3ecb3fb3d558f96581068a61043b00140d
yongwang1991 <yongwang1991@gmail.com>
2024-11-01 21:08:27 +0800

    updated the about us and methodology pages

--- pages/01 About_Us.py
+++ pages/01 About_Us.py
@@ -9,23 +9,21 @@ st.set_page_config(
 
 st.title("About this App")
 
-st.write("This is a Specifications Drafter which is designed to help PMTs to come out with the first draft of the specifications for the purchase. We hope it will speed up the procurement process for you.")
+st.write("This is a Requirements Specifications Drafter which is designed to help Users/PMTs to develop the first draft of the specifications for their Tender/Quotation.")
 
 with st.expander("Project Scope"):
-    st.write("To address the challenges PMTs face in drafting requirement specifications, an automated Requirement Specifications Generator is created to reduce the time spent significantly.")
+    st.write("To address the challenges Users/PMTs face in drafting their requirement specifications, an automated Requirement Specifications Generator is created to reduce the time spent to develop the first draft significantly.")
              
 with st.expander("Project Objectives"):
-    st.write("1. Reduce Lead Time: Minimise time taken to draft requirement specifications.")
-    st.write("2. User-Friendly Interface: Provide an intuitive interface so that PMTs to easily generate specifications.")
-    st.write("3. Enhance Consistency: Ensure uniformity to draft specifications across different projects.")
+    st.write("1. Provide Guidance: To provide guidance for Users/PMTs to develop the first draft of their requirements specifications.")
+    st.write("2. Reduce Lead Time: Minimise time taken to develop the draft requirement specifications.")
+    st.write("3. User-Friendly Interface: Provide an intuitive interface so that Users/PMTs can easily generate the requirements specifications.")
 
 
 with st.expander("Data Sources"):
-    st.write("1. Enter your prompt in the text area.")
+    st.write("1. NA. The idea is to tap on the wide training data of the LLM to develop the requirements specifications.")
 
 with st.expander("Features"):
-    st.write("1. Enter your prompt in the text area. For example, enter the following: Draft the specifications for establishing a 3 years period contract for purchasing movie tickets.")
-    st.write("2. Click the 'Submit' button.")
-    st.write("3. The app will generate the draft specfications based on your prompt.")
-    st.write("4. If the generated specifications require improvements, add a liner or two to refine the prompt and try again.")
-    st.write("5. It is that simple. Give it a try!")
+    st.write("1. Generative Ability. The app is able to generate the Requirements Specifcations based on user prompt, without any additional data provided.")
+    st.write("2. Context Awareness. The app is able to generate specifications for the specific section of the Requirements Specifications.")
+    st.write("3. User Feedback Mechanism. The app is able to modify or amend its replies based on the user feedback.")
